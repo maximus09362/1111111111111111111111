6a9e778fcf62a6c53baba0cd3d7caf818c6d7a71
MAX <MAXIMILIANO.EL000@GMAIL.COM>
2026-02-13 22:17:39 -0300

    fix page

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,15 @@
-import Image from "next/image";
-
 export default function Page() {
   return (
     <main
       style={{
         width: "100%",
         height: "100vh",
-        position: "relative",
-        margin: 0,
-        padding: 0,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
       }}
     >
-      <Image
-        src="/mantenimiento.png"
-        alt="Sitio en mantenimiento"
-        fill
-        style={{ objectFit: "cover" }}
-        pri
+      <h1>Modo mantenimiento activo</h1>
+    </main>
+  );
+}
